9ff8e4f78cba334cedc7d28a2e6407406e29db11
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-03-18 11:00:10 -0300

    vanillajs quiz was updated

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -14,16 +14,16 @@ function IndexPage() {
 <meta name="description" content="" />
 
 <meta property="og:type" content="website" />
-<meta property="og:url" content="https://vanillajs-quiz-felipefeitosadev.vercel.app/" />
+<meta property="og:url" content="https://vanillajavascript-quiz-felipefeitosadev.vercel.app/" />
 <meta property="og:title" content="QUIZ - Vanilla JS" />
 <meta property="og:description" content="" />
-<meta property="og:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/vanillajs-quiz/32d57f8cd407ae3368e8166927b93741da6aa283/quiz-vanillaJs_layout2_vector.svg" />
+<meta property="og:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/vanillajavascript-quiz/852d1fdeb7c80231b229e8a7e418e3def912d76a/quiz-vanillaJs_layout2_vector.svg" />
 
 <meta property="twitter:card" content="summary_large_image" />
-<meta property="twitter:url" content="https://vanillajs-quiz-felipefeitosadev.vercel.app/" />
+<meta property="twitter:url" content="https://vanillajavascript-quiz-felipefeitosadev.vercel.app/" />
 <meta property="twitter:title" content="QUIZ - Vanilla JS" />
 <meta property="twitter:description" content="" />
-<meta property="twitter:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/vanillajs-quiz/32d57f8cd407ae3368e8166927b93741da6aa283/quiz-vanillaJs_layout2_vector.svg"></meta>
+<meta property="twitter:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/vanillajavascript-quiz/852d1fdeb7c80231b229e8a7e418e3def912d76a/quiz-vanillaJs_layout2_vector.svg"></meta>
 
       </Head>
       
