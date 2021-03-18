0755f4eab895e1dea8a599360437b4b7b1c2a6c7
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-03-18 10:36:30 -0300

    vanillajs quiz was updated

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -6,24 +6,24 @@ function IndexPage() {
 
       <Head>
        <link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,400;0,700;1,100&display=swap" rel="stylesheet" />
-        <title>QUIZ - VANILLA JS</title>
-        <meta name="title" content="QUIZ - DAFT PUNK" />
-        <meta name="description" content=""/>
+       <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,400;0,700;1,100&display=swap" rel="stylesheet" />
+       <title>QUIZ - VANILLA JS</title>
+       
 
+<meta name="title" content="QUIZ - Vanilla JS" />
+<meta name="description" content="" />
 
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://daftpunk-quiz-felipefeitosadev.vercel.app/"/>
-        <meta property="og:title" content="QUIZ - DAFT PUNK"/>
-        <meta property="og:description" content=""/>
-        <meta property="og:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/daftpunk-quiz/main/daft-punk-thumb.png"/>
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://vanillajs-quiz-felipefeitosadev.vercel.app/" />
+<meta property="og:title" content="QUIZ - Vanilla JS" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/vanillajs-quiz/32d57f8cd407ae3368e8166927b93741da6aa283/quiz-vanillaJs_layout2_vector.svg" />
 
-
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://daftpunk-quiz-felipefeitosadev.vercel.app/"/>
-        <meta property="twitter:title" content="QUIZ - DAFT PUNK"/>
-        <meta property="twitter:description" content=""/>
-        <meta property="twitter:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/daftpunk-quiz/main/daft-punk-thumb.png"/>
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://vanillajs-quiz-felipefeitosadev.vercel.app/" />
+<meta property="twitter:title" content="QUIZ - Vanilla JS" />
+<meta property="twitter:description" content="" />
+<meta property="twitter:image" content="https://raw.githubusercontent.com/FelipeFeitosaDev/vanillajs-quiz/32d57f8cd407ae3368e8166927b93741da6aa283/quiz-vanillaJs_layout2_vector.svg"></meta>
 
       </Head>
       
