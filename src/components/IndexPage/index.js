31b2a90aa2e96154ab23ea43b7231c7ba5925ec9
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-03-19 11:12:27 -0300

    og:image was created

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -1,5 +1,6 @@
 
 import Head from 'next/head'
+import db from '../../../db.json';
 
 function IndexPage() {
   return (
@@ -7,8 +8,22 @@ function IndexPage() {
       <Head>
        <link rel="preconnect" href="https://fonts.gstatic.com" />
        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,400;0,700;1,100&display=swap" rel="stylesheet" />
-       <title>QUIZ - VANILLA JS</title>
+       
+<title>QUIZ - Vanilla JS</title>
+<meta name="title" content="QUIZ - Vanilla JS" />
+<meta name="description" content="" />
 
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://quiz-vanillajs-felipefeitosadev.vercel.app/" />
+<meta property="og:title" content="QUIZ - Vanilla JS" />
+<meta property="og:description" content="" />
+<meta property="og:image" content={db.bg} />
+
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://quiz-vanillajs-felipefeitosadev.vercel.app/" />
+<meta property="twitter:title" content="QUIZ - Vanilla JS" />
+<meta property="twitter:description" content="" />
+<meta property="twitter:image" content={db.bg} />
       </Head>
       
   )
